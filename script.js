577a22b35626224694345a0ae4a5b5917a4dc811
Zhangirrr <zangiruakasov@gmail.com>
2022-11-23 00:41:33 +0600

    the final version

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
-let select = document.querySelector("#select_rovers")
-let selectCameras = document.querySelector("#select_cameras")
-let input = document.querySelector("#input_sol")
-let searchButton = document.querySelector("#btn")
-
+let allProducts = document.querySelector("#all_products")
+let productsDiv = document.querySelector(".products")
+let button = document.querySelector("#btn")
 
 ;(async() => {
-    let response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/?&api_key=9YAmT545pMTnfCAtpF7daBTLAax7GebgtATViczx')
+    let response = await fetch('https://dummyjson.com/products')
     let data = await response.json();
-        for(let rover of data.rovers){
-            select.innerHTML += `<option value="${rover.name}">${rover.name}</option>`
+        for(let products of data.products){
+            allProducts.innerHTML += `
+            <div class="products">
+                <div class ="image" style="background-image: url(${products.images[0]});">
+
+                </div>
+                <br><br>
+                <span class="title">${products.title}</span><hr>
+                <span class="price">${products.price}$</span><br><br>
+                <span class="title">Category: </span>${products.category}<br><br>
+                <button id="btn" onClick="sendId(${products.id})">Add to cart</button><br><br>
+            </div>`
         }
-        drawComponents(data.rovers[0].id)
-        // console.log(data)
-    return data;
 
 })();
 
-async function drawComponents(optionId){
-    let response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/?&api_key=9YAmT545pMTnfCAtpF7daBTLAax7GebgtATViczx')
-    let data = await response.json();
 
-    selectCameras.innerHTML = "";
 
-    for(let rover of data.rovers){
-        for(let camera of rover.cameras){
-            if(optionId === camera.rover_id){
-                selectCameras.innerHTML += `<option value="${camera.name}">${camera.name}</option>`
-            }
-        }
-    }
+function sendId(productId){
+    let arr = localStorage.getItem("key") || [];
+    localStorage.setItem("key", [...arr, productId])
 }
-
-select.addEventListener("change", () => {
-    drawComponents(+select.value);
-})
-
-searchButton.addEventListener("click", async function() {
-
-    let divResult = document.querySelector("#result")
-
-
-    let photoResponse = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${select.value.toLowerCase()}/photos?sol=${+input.value}&camera=${selectCameras.value.toLowerCase()}&api_key=9YAmT545pMTnfCAtpF7daBTLAax7GebgtATViczx`)
-    let photoData = await photoResponse.json();    
-    for(let photo of photoData.photos){
-        divResult.innerHTML += `<img src="${photo.img_src}" style="width: 300px; height: 300px">`;
-    }
-
-})
-
 
 
 
