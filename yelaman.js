82b66cc29678b068896b6291cdc2745f732d0389
Еламан Сураншиев <@itstep.loc>
2022-11-23 21:37:02 +0600

    done

--- yelaman.js
+++ yelaman.js
@@ -6,7 +6,7 @@
 
     let data = await response.json();
     console.log(data);
-    
+
     let cart_mass = [];
 
     cart.map(id_num => cart_mass.push(data.products.filter(product => product.id == id_num)[0]));
@@ -15,7 +15,6 @@
     let container = document.querySelector("#container");
     cart_mass.map(product => container.innerHTML += `
         <p>${product.title}</p>
-        <img src="${product.images}">
         <p>${product.price}</p>
         <p>${product.category}</p>
         <img src="${product.images}" width="200px">
