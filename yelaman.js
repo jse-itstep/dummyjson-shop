b6841935846c2fd772af1b54cb2db403cde02a02
Еламан Сураншиев <@itstep.loc>
2022-11-23 21:36:10 +0600

    done

--- yelaman.js
+++ yelaman.js
@@ -1,10 +1,12 @@
 ;(async () => {
-
     let cart = JSON.parse(localStorage.getItem("key")) || [];
     console.log(cart);
+
     let response = await fetch("https://dummyjson.com/products");
+
     let data = await response.json();
     console.log(data);
+    
     let cart_mass = [];
 
     cart.map(id_num => cart_mass.push(data.products.filter(product => product.id == id_num)[0]));
@@ -14,5 +16,8 @@
     cart_mass.map(product => container.innerHTML += `
         <p>${product.title}</p>
         <img src="${product.images}">
+        <p>${product.price}</p>
+        <p>${product.category}</p>
+        <img src="${product.images}" width="200px">
         `);
 })();
